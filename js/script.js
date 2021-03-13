2b709733d1ba5a9958713df85eed40c3b7e7e74b
Sourabh Suneja <sourabhsuneja021@gmail.com>
2021-03-13 18:17:17 +0530

    Quiz questions updated

--- js/script.js
+++ js/script.js
@@ -1,13 +1,53 @@
 // declare questions to be asked
 const questions = [{
-            q: "What is 10 + 10 equal to?",
-            options: ['25', '30', '20', '40'],
-            correct: '20'
+            q: "What is Sourabh's favourite fast food?",
+            options: ['Samosa', 'Chaat Papdi', 'Dosa', 'French Fries'],
+            correct: 'Dosa'
         },
         {
-            q: "What is Athena's favorite animal?",
-            options: ['Dog', 'Cat', 'Squirrel', 'Parrot'],
-            correct: 'Dog'
+            q: "If there's one thing that irritates Sourabh, what could that be most probably?",
+            options: ['Neighbours', 'The Education System', 'Attending a party', 'Inability to write'],
+            correct: 'The Education System'
+        },
+        {
+            q: "Suppose that Sourabh has to bribe someone in order to obtain a driving license. Will Sourabh go ahead?",
+            options: ['Yes', 'No', 'Can\'t say', 'Depends on his mood'],
+            correct: 'No'
+        },
+        {
+            q: "Does Sourabh love to listen to very loud music?",
+            options: ['Yes', 'No', 'Can\'t say', 'Depends on his mood'],
+            correct: 'No'
+        },
+        {
+            q: "Is Sourabh short-tempered?",
+            options: ['Yes', 'No', 'Little bit', 'Only when he feels down'],
+            correct: 'Only when he feels down'
+        },
+        {
+            q: "What is Sourabh's date of birth?",
+            options: ['16 Dec', '15 Dec', '5 Jan', '23 Nov'],
+            correct: '16 Dec'
+        },
+        {
+            q: "If Sourabh was asked to stay alone at home for an entire day, how would he react?",
+            options: ['Refuse bluntly', 'Accept happily', 'Request his friends to come', 'Frown his face'],
+            correct: 'Accept happily'
+        },
+        {
+            q: "Is Sourabh hygiene-conscious?",
+            options: ['Too much', 'Little bit', 'Not at all', 'Can\'t say'],
+            correct: 'Too much'
+        },
+        {
+            q: "What does Sourabh value the most among all these?",
+            options: ['Money', 'Relationships', 'Status', 'Knowledge'],
+            correct: 'Knowledge'
+        },
+        {
+            q: "If Sourabh plans to settle abroad, where is he mostly likely to go?",
+            options: ['US', 'Britain', 'Canada', 'Australia'],
+            correct: 'US'
         }
     ]
     // track score and the question currently in view
